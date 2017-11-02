f24a531e15dacb82accbbf02097189bf71cf44ec
mdb <mdb@mdblabs.com>
2017-11-03 00:00:36 +0100

    Main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by mariano on 2/11/17.
+ */
+
+function init() {
+    // code here.
+
+
+    var stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    stage.update();
+}
+
+$('#toggle').click(function () {
+    screenfull.toggle($('#demoCanvas')[0]);
+});
