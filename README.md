f326e7245da6a59cd2248d0f5c3c51f16337d6fc
mdb <mdb@mdblabs.com>
2017-11-04 21:47:41 +0100

    Fullscreen OK

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 ## One GitHub GameOff Entry
 
 ## Credits
--- js/main.js
+++ js/main.js
@@ -2,83 +2,114 @@
  * Created by mariano on 2/11/17.
  */
 
+var KEYCODE_LEFT = 37,
+    KEYCODE_RIGHT = 39,
+    KEYCODE_UP = 38,
+    KEYCODE_DOWN = 40,
+    KEYBOARD_ESC = 27;
+
 var stage;
 var bg;
+var fg;
+var text;
+var tFly;
 
-window.addEventListener('resize', resize, false);
+var canvas;
+var wOriginal = 500;
+var hOriginal = 375;
+var reload = false;
 
 function init(){
     // code here.
-    stage = new createjs.Stage("game_canvas");
 
+    canvas = document.getElementById("game_canvas");
+    stage = new createjs.Stage("game_canvas");
     createBackground();
-    createjs.Ticker.addEventListener(this);
-    alert("Test");
-    resize();
+
+    createjs.Ticker.addEventListener("tick",ticUpdate);
+
 }
 
 function createBackground() {
+
     bg = new createjs.Shape();
+    fg = new createjs.Shape();
+    text = new createjs.Text();
+    tFly = new createjs.Text();
+
+    text.font = "24px VT323";
+    tFly.font = "18px VT323";
+
+    text.color = "#ade67e";
+    tFly.color = "#ade67e";
+    text.x = 5;
+    text.y = 5;
+    text.text = "DEFCON : 1983";
+    tFly.text = String.fromCharCode(9611);
+    //Ancho del texto:11
+    tFly.x = 141;
+    tFly.y = 9;
+
+    //bg.graphics.clear()
+    //bg.graphics.beginFill("black").drawRect(0,0,stage.canvas.width,stage.canvas.height);
+
     stage.addChild(bg);
+    stage.addChild(fg);
+    stage.addChild(text);
+    stage.addChild(tFly);
 }
 
 
-function toggleFullScreen() {
-    var canvas = document.getElementById("game_canvas");
-    if ((document.fullScreenElement && document.fullScreenElement !== null) ||
-        (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-        if (document.documentElement.requestFullScreen)
-        {
-
-            //document.documentElement.requestFullScreen();
-            canvas.requestFullScreen();
-            //canvas.width = screen.width;
-            //canvas.height = screen.height;
-
-
-        } else if (document.documentElement.mozRequestFullScreen)
-        {
+function fullscreenON() {
+    if (
+        document.fullscreenEnabled ||
+        document.webkitFullscreenEnabled ||
+        document.mozFullScreenEnabled ||
+        document.msFullscreenEnabled
+    ){
+        canvas = document.getElementById("game_canvas");
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen();
+        } else if (canvas.mozRequestFullScreen) {
             canvas.mozRequestFullScreen();
-            //canvas.width = screen.width;
-            //canvas.height = screen.height;
+        } else if (canvas.msRequestFullscreen) {
+            canvas.msRequestFullscreen();
+        }
 
 
-        } else if (document.documentElement.webkitRequestFullScreen)
-        {
-            canvas.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-            //canvas.width = screen.width;
-            //canvas.height = screen.height;
-        }
-    } else {
-        if (document.cancelFullScreen)
-        {
-            canvas.cancelFullScreen();
-            //canvas.width = 500;
-            //canvas.height = 375;
-        } else if (document.mozCancelFullScreen)
-        {
-            canvas.mozCancelFullScreen();
-            //canvas.width = 500;
-            //canvas.height = 375;
-        } else if (document.webkitCancelFullScreen)
-        {
-            canvas.webkitCancelFullScreen();
-            //canvas.width = 500;
-            //canvas.height = 375;
-        }
     }
+
+
+}
+function fullscreenOFF()
+{
 }
 
+function ticUpdate(){
 
-function tick() {
+
+    canvas = document.getElementById("game_canvas");
+
+    if(document.fullScreenElement==null){
+
+        canvas.width = wOriginal;
+        canvas.height = hOriginal;
+
+    }
+    else
+    {
+        canvas.width = screen.width;
+        canvas.height = screen.height;
+    }
+    bg.graphics.clear();
+    fg.graphics.clear();
+
+    bg.graphics.beginFill("black").drawRect(0,0,canvas.width,canvas.height);
+    fg.graphics.setStrokeStyle(2).beginStroke("#ade67e").drawRect(0,0,canvas.width,canvas.height);
+
+
+
     stage.update();
-}
-
-function resize() {
-    stage.canvas.width = window.innerWidth;
-    stage.canvas.height = window.innerHeight;
-
-    bg.graphics.clear()
-    bg.graphics.beginFill("red").drawRect(0,0,stage.canvas.width,stage.canvas.height);
-
 }
